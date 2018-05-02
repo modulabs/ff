103ff967ff27078c42819087df679977f9ff6141
hihi <pes8793@gmail.com>
2018-05-02 16:18:31 +0900

    some pixel test

---
+++ pes/thresholdTest/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  2 14:47:46 2018
+
+@author: eunsoo
+"""
+
+import cv2
+
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+#%%
+img_syn = cv.imread('Tp_D_CNN_M_N_ani00057_ani00055_11149.jpg')
+img_org = cv.imread('Au_ani_00057.jpg')
